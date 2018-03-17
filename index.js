0010d4c840841d671a4580e995951e15c42ac839
Alex Edwards <info@acedigitalagency.net>
2018-03-17 21:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!';
 }
+function happyHolidaysTo(holiday, name) {
+  return 'Happy ${holidays}, ${name}!';
+}
 function holidayCountdown(days, holiday) {
   return 'It\'s ${days} days until ${holiday}!';
 }
