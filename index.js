b512995b39fef59ef61199862b3e397557747ef2
Alex Edwards <info@acedigitalagency.net>
2018-03-17 21:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
  return "Happy holidays!";
 }
-function happyHolidaysTo (name) {
+function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!';
 }
-function holidayCountdown (days, holiday) {
+function holidayCountdown(days, holiday) {
   return 'It\'s ${days} days until ${holiday}!';
 }
